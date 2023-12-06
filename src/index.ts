19568a0aac2965cb5c9399aec033de436dceb652
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-12-07 01:52:06 +0300

    connect database and prisma

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-import express from 'express';
-import http from 'http';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import compression from 'compression';
+import { app } from "./server";
 
-const app = express();
-
-app.use(cors({
-    credentials: true,
-}))
-
-app.use(compression());
-app.use(bodyParser.json());
-
-app.use(cookieParser());
-
-
-
-
-
-const server = http.createServer(app);
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(3000, () => {
+    console.log("Server started");
+    })
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import compression from "compression";
+
+export const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World" });
+});
+
